b2b5e01d1315d18ea2baaae29fe3308a4ed75e8c
Jaemin Han <jaemin.han@nyu.edu>
2016-11-30 18:26:24 -0500

    working process

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,5 @@
 const userRouter = require('express').Router();
-const {
-      getPassword,
-      createUser 
-      } = require('../models/user.js')
+const { getPassword, createUser } = require('../models/user.js')
 
 userRouter.route('/login')
   .post(getPassword, (req, res) => res.json({message: 'logged in'}));
